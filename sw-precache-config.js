0578f825c256210f88fd30ef83b0563bb5d15910
jvitor83 <jvitor83@hotmail.com>
2016-11-30 20:05:15 -0300

    ServiceWorker review dynamic include

---
+++ sw-precache-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  navigateFallback: '/index.html',
+  stripPrefix: 'www',
+  root: 'www/',
+  staticFileGlobs: [
+    'www/index.html',
+    'www/*.html',
+    'www/**/*.html',
+    'www/**/*.js',
+    'www/**/*.xml',
+    'www/**/*.css',
+    'www/**/*.json',
+    'www/**/*.ico',
+    'www/**/*.png',
+    'www/**/*.otf',
+    'www/**/*.eot',
+    'www/**/*.svg',
+    'www/**/*.ttf',
+    'www/**/*.woff',
+    'www/**/*.woff2'
+  ]
+};
