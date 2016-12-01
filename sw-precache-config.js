dc8bbd8115cf75ec1aabacfd9b02689c9ab35c32
jvitor83 <jvitor83@hotmail.com>
2016-12-01 13:38:22 -0300

    Better offline cache improvements

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -2,21 +2,9 @@ module.exports = {
   navigateFallback: '/index.html',
   stripPrefix: 'www',
   root: 'www/',
+  ignoreUrlParametersMatching: [/./],
   staticFileGlobs: [
     'www/index.html',
-    'www/*.html',
-    'www/**/*.html',
-    'www/**/*.js',
-    'www/**/*.xml',
-    'www/**/*.css',
-    'www/**/*.json',
-    'www/**/*.ico',
-    'www/**/*.png',
-    'www/**/*.otf',
-    'www/**/*.eot',
-    'www/**/*.svg',
-    'www/**/*.ttf',
-    'www/**/*.woff',
-    'www/**/*.woff2'
+    'www/**/!(*.map|service-worker.js)'
   ]
 };
