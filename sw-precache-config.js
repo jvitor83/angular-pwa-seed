104cda2004d8a3b5cc79e6447ffd2ec6faa1d9b7
jvitor83 <jvitor83@hotmail.com>
2016-12-01 21:46:16 -0300

    Web files in glob to offline app

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,9 +5,16 @@ module.exports = {
   ignoreUrlParametersMatching: [/./],
   handleFetch: true,
   verbose: true,
-  maximumFileSizeToCacheInBytes: 10485760,
+  maximumFileSizeToCacheInBytes: 104857600,
   staticFileGlobs: [
     'www/index.html',
-    'www/**/*(!(*.map))'
+    //'www/**/*(!(*.map))', //every file except sourcemaps (all files should download successfully to serviceworker to works, otherwise it will not install the serviceworker (and offline will not work). Because of that, all files should have mimetype configured at server to be downloaded.)
+    "www/**/*.json",
+    "www/**/*.css",
+    "www/**/*.{eot,ttf,woff,woff2,eof,otf}",
+    "www/**/*.js",
+    "www/**/*.xml",
+    "www/**/*.{htm,html,xhtml,cshtml,jhtml,shtml}",
+    "www/**/*.{png,jpg,jpeg,gif,svg,mp3,ico}"
   ]
 };
