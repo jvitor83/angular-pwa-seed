2553a4b3080b975240d0c71e4c78bae0aa4746ea
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2017-03-27 16:11:19 -0400

    google identity

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,21 +3,32 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
+
 export const environment = {
   production: false,
   authentication: {
-    authority: 'http://idp-teste.tjmt.jus.br',
-    client_id: '2380',
-    redirect_uri: 'http://localhost:5555/callback.html',
-    post_logout_redirect_uri: 'http://localhost:5555/',
-    response_type: 'code id_token token',
-    scope: 'openid email roles',
+    authority: 'https://accounts.google.com',
+    client_id: '929544282709-8mkof7adolh13fnualb2f4f176mjd2ot.apps.googleusercontent.com',
+    client_secret: '2F88K5gbFOUhUuhSl7Z-XbD9',
+    redirect_uri: 'https://angular-pwa-seed.netlify.com/callback.html',
+    post_logout_redirect_uri: 'https://angular-pwa-seed.netlify.com/',
+    response_type: 'token id_token',
+    scope: 'openid profile',
 
-    silent_redirect_uri: 'http://localhost:5555/silentrefreshframe.html',
+    silent_redirect_uri: 'https://angular-pwa-seed.netlify.com/silentrefreshframe.html',
     automaticSilentRenew: true,
     //silentRequestTimeout:10000,
 
     filterProtocolClaims: true,
-    loadUserInfo: true
+    loadUserInfo: true,
+
+    metadata: {
+      issuer: "https://accounts.google.com",
+      authorization_endpoint: "https://accounts.google.com/o/oauth2/v2/auth",
+      token_endpoint: "https://www.googleapis.com/oauth2/v3/tokeninfo",
+      userinfo_endpoint: "https://www.googleapis.com/oauth2/v3/userinfo",
+      jwks_uri: "https://www.googleapis.com/oauth2/v3/certs",
+    }
+
   }
 };
