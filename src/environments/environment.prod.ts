0db60b283885f8cca717b0c160d988b0b4b3d238
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2017-03-27 16:48:48 -0400

    no authority

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@
 export const environment = {
   production: true,
   authentication: {
-    authority: 'https://accounts.google.com',
+    //authority: 'https://accounts.google.com',
     client_id: '929544282709-8mkof7adolh13fnualb2f4f176mjd2ot.apps.googleusercontent.com',
     client_secret: '2F88K5gbFOUhUuhSl7Z-XbD9',
     redirect_uri: 'https://angular-pwa-seed.netlify.com/callback.html',
@@ -30,6 +30,6 @@ export const environment = {
       "revocation_endpoint": "https://accounts.google.com/o/oauth2/revoke",
       "jwks_uri": "https://www.googleapis.com/oauth2/v3/certs",
     }
-    
+
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@
 export const environment = {
   production: false,
   authentication: {
-    authority: 'https://accounts.google.com',
+    //authority: 'https://accounts.google.com',
     client_id: '929544282709-8mkof7adolh13fnualb2f4f176mjd2ot.apps.googleusercontent.com',
     client_secret: '2F88K5gbFOUhUuhSl7Z-XbD9',
     redirect_uri: 'https://angular-pwa-seed.netlify.com/callback.html',
