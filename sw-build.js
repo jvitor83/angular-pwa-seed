d7547d6310e4753b13ca128a27bf0fc633d84943
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2017-03-27 14:20:40 -0400

    netlify test

--- sw-build.js
+++ sw-build.js
@@ -8,9 +8,6 @@ var data = fs.readFileSync(file); //read existing contents into data
 var fd = fs.openSync(file, 'w+');
 var buffer = new Buffer("const CACHE_VERSION = '"+t+"';");
 
-var ws = fs.writeSync(fd, buffer, 0, buffer.length, 0); //write new data
-fs.appendFile(fd, data);
+fs.writeSync(fd, buffer, 0, buffer.length, 0); //write new data
+fs.writeSync(fd, data, 0, data.length, buffer.length);
 fs.close(fd);
-
-console.log('ws');
-console.log(ws);
