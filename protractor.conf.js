4190afbf6f8a8345b8131934ca226099d7843903
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2017-08-07 13:35:28 -0400

    test fixes

--- protractor.conf.js
+++ protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://localhost:5555/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
