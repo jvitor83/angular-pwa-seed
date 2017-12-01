e58badaf4fe3531b5ff9cb4fc2a75278c2f051a4
João Vitor Paes de Barros do Carmo <joao.carmo@tjmt.jus.br>
2017-12-01 17:48:49 -0300

    Animations improvements

--- src/app/fade.animations.ts
+++ src/app/fade.animations.ts
@@ -1,18 +1,32 @@
-import {trigger, animate, style, group, animateChild, query, stagger, transition} from '@angular/animations';
+import { trigger, animate, style, group, animateChild, query, stagger, transition } from '@angular/animations';
 
+// Fade Animation between router
 export const routerTransition = trigger('routerTransition', [
-  transition('* <=> *', [
-    /* order */
-    /* 1 */ query(':enter, :leave', style({ position: 'fixed' })
-      , { optional: true }),
-    /* 2 */ group([  // block executes in parallel
-      query(':enter', [
-        animate('0.3s', style({ opacity: 1 }))
-      ], { optional: true }),
-      query(':leave', [
-        animate('0.1s', style({ opacity: 0 }))
-      ], { optional: true }),
-    ])
+  transition('* => *', [
+    query(':leave', style({ position: 'absolute', left: 0, right: 0, opacity: 1, 'padding-left': '16px', 'padding-right': '16px' })),
+    query(':enter', style({ position: 'absolute', left: 0, right: 0, opacity: 0, 'padding-left': '16px', 'padding-right': '16px' })),
+    query(':leave', animate('0.2s', style({ opacity: 0 }))),
+    query(':enter', animate('0.4s', style({ opacity: 1 })))
   ])
+]);
 
-])
+
+
+// // Slide Animation between router
+// export const routerTransition = trigger('routerTransition', [
+//   transition('* <=> *', [
+//     /* order */
+//     /* 1 */ query(':enter, :leave', style({ position: 'fixed', width: '100%', 'padding-left': '16px', 'padding-right': '16px' })
+//       , { optional: true }),
+//     /* 2 */ group([  // block executes in parallel
+//       query(':enter', [
+//         style({ transform: 'translateX(100%)' }),
+//         animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+//       ], { optional: true }),
+//       query(':leave', [
+//         style({ transform: 'translateX(0%)' }),
+//         animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+//       ], { optional: true }),
+//     ])
+//   ])
+// ]);
