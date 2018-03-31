0140b229965a160d51f18502c7a723ff88127591
kirill <kirimihi@rambler.ru>
2018-03-31 22:34:20 +0300

    renaming

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,37 @@
-import { MenuController } from 'ionic-angular';
+@@ -2,13 +2,35 @@ import { MenuController } from 'ionic-angular';
 import { Component, OnInit, ApplicationRef, AfterViewInit, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 
+// import LoadImage from 'blueimp-load-image';
+// import {PAGES, HEADER_HEIGHT} from '../shared/constants';
+// import {setLiveCamera} from '../shared/config';
+// import {showPage} from '../shared/helpers';
+// import AnnotatePage from './annotate';
+// import AboutPage from './about';
+
+let inputPhoto = document.getElementById('file-input');
+
 @Component({
     moduleId: module.id,
-    templateUrl: './dashboard.component.html'
+    templateUrl: './dashboard.component.html',
+    selector: 'my-app',
+	template: `
+	  <div>
+	    <input type="file" (change)="onChange($event)"/>
+	  </div>
+	`,
 })
+
 export class DashboardComponent {
+
 
     constructor(public menu: MenuController, public zone: NgZone, public application: ApplicationRef) {
     }
     
+
+onChange(event) {
+    console.log('onChange');
+    var files = event.srcElement.files;
+    console.log(files);
+  }
 }
