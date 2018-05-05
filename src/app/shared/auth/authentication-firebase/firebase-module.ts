ed970c489d5f73f1e99a287187910c7f2b2a8d0a
jvitor83 <jvitor83@hotmail.com>
2018-05-05 03:33:43 -0400

    upgrade packages

--- src/app/shared/auth/authentication-firebase/firebase-module.ts
+++ src/app/shared/auth/authentication-firebase/firebase-module.ts
@@ -5,13 +5,14 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AUTHENTICATION_SERVICE } from './../authentication/authentication-service.token';
 import { FirebaseAppConfig, AngularFireModule } from 'angularfire2';
 import { NgModule, ModuleWithProviders, Type } from '@angular/core';
+import { FirebaseOptions } from '@firebase/app-types';
 
 
 
 
 @NgModule()
 export class FirebaseAuthModule {
-  static forRoot(authenticationSettings: FirebaseAppConfig, provide = AUTHENTICATION_SERVICE) {
+  static forRoot(authenticationSettings: FirebaseOptions, provide = AUTHENTICATION_SERVICE) {
     return <(any[] | Type<any> | ModuleWithProviders)[]>[
       AngularFireDatabaseModule,
       AngularFireAuthModule,
