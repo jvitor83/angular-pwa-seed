02d16a337a5186baf44f11014eaac0057f4ca92c
jvitor83 <jvitor83@hotmail.com>
2018-05-11 00:14:21 -0400

    updates and fixes

--- src/app/fade.animations.ts
+++ src/app/fade.animations.ts
@@ -3,8 +3,8 @@ import { trigger, animate, style, group, animateChild, query, stagger, transitio
 // Fade Animation between router
 export const routerTransition = trigger('routerTransition', [
   transition('* => *', [
-    query(':leave', style({ position: 'absolute', left: 0, right: 0, opacity: 1, 'padding-left': '16px', 'padding-right': '16px' }), { optional: true }),
-    query(':enter', style({ position: 'absolute', left: 0, right: 0, opacity: 0, 'padding-left': '16px', 'padding-right': '16px' }), { optional: true }),
+    query(':leave', style({ position: 'absolute', left: 0, right: 0, opacity: 1 }), { optional: true }),
+    query(':enter', style({ position: 'absolute', left: 0, right: 0, opacity: 0 }), { optional: true }),
     query(':leave', animate('0.2s', style({ opacity: 0 })), { optional: true }),
     query(':enter', animate('0.4s', style({ opacity: 1 })), { optional: true })
   ])
@@ -16,7 +16,7 @@ export const routerTransition = trigger('routerTransition', [
 // export const routerTransition = trigger('routerTransition', [
 //   transition('* <=> *', [
 //     /* order */
-//     /* 1 */ query(':enter, :leave', style({ position: 'fixed', width: '100%', 'padding-left': '16px', 'padding-right': '16px' })
+//     /* 1 */ query(':enter, :leave', style({ position: 'fixed', width: '100%' })
 //       , { optional: true }),
 //     /* 2 */ group([  // block executes in parallel
 //       query(':enter', [
